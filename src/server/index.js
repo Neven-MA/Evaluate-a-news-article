5f9baf1e4d8edeef278527eb4fc5eb969153e5a7
Neven-MA <nevenforever@hotmail.com>
2021-08-28 21:25:56 +0200

    fifth

--- src/server/index.js
+++ src/server/index.js
@@ -40,23 +40,6 @@ app.post("/url-Analyzer", async (req, res) => {
     console.log("error", error);
   }
 });
-// a route that handling post request for new URL that coming from the frontend
-/* TODO:
-    1. GET the url from the request body
-    2. Build the URL it should be something like `${BASE_API_URL}?key=${MEAN_CLOUD_API_KEY}&url=${req.body.url}&lang=en`
-    3. Fetch Data from API
-    4. Send it to the client
-    5. REMOVE THIS TODO AFTER DOING IT 😎😎
-    server sends only specified data to the client with below codes
-     const sample = {
-       text: '',
-       score_tag : '',
-       agreement : '',
-       subjectivity : '',
-       confidence : '',
-       irony : ''
-     }
-*/
 
 app.get("/test", function (req, res) {
   res.send(mockAPIResponse);
